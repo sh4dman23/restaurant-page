28366bcd73d7e702336467f0678f84f31b73fc03
Shadman <sh4dman0001@gmail.com>
2023-11-12 17:46:29 +0600

    Apply minor bug fix
    Menu indicator for current tab didn't work when user went to 'Menu' by
    clicking on the button in the homepage. Fixed this bug.

--- src/main.js
+++ src/main.js
@@ -16,17 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (target.classList.contains('nav-item') || target.classList.contains('discover-button')) {
             clearMain();
+            let activeTab;
 
             if (target.id === 'home') {
+                activeTab = target;
                 loadHome();
             } else if (target.id === 'menu' || target.classList.contains('discover-button')) {
+                activeTab = document.querySelector('#menu');
                 loadMenu();
             } else if (target.id === 'contacts') {
+                activeTab = target;
                 loadContacts();
             }
 
             document.querySelectorAll('.nav-item').forEach(element => element.classList.remove('active'));
-            target.classList.add('active');
+            if (activeTab) {
+                activeTab.classList.add('active');
+            }
         }
     });
 });
